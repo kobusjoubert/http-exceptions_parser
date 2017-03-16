08586a73070a097229ae2fc5da57d7b068fb166d
Kobus Joubert <kobus@translate3d.com>
2017-03-16 20:36:49 +0200

    Regular expresttions to extract response code and body out of message

--- lib/http/exceptions_parser/parser.rb
+++ lib/http/exceptions_parser/parser.rb
@@ -4,11 +4,15 @@ module Http
       attr_reader :response, :original_exception
 
       def initialize(msg)
-        code = 200
-        body = msg
+        msg_code  = /Status (\d{1,})/.match(msg)
+        msg_body  = /\n(.*)( Original Exception: )/.match
+        msg_body  = /\n(.*)/.match(msg) unless msg_body
+        code      = msg_code[1].to_i if msg_code
+        body      = msg_body[1] if msg_body
+        @response = OpenStruct.new(code: code, body: body)
 
-        @response           = OpenStruct.new(code: code, body: body)
-        @original_exception = ''
+        original_exception  = /Original Exception: (.*)/.match(msg)
+        @original_exception = original_exception[1] if original_exception
       end
     end
   end
